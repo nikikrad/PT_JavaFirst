aba51a3d762e699bca8764b7aacbcb5a900f5d8a
NiKiKRaD <nikikrad228@gmail.com>
2022-01-31 13:33:09 +0300

    ничего не сделал

--- src/firstlaba/Main.java
+++ src/firstlaba/Main.java
@@ -6,19 +6,16 @@ import java.util.Scanner;
 
 public class Main {
 
-
-
     public static void main(String[] args){
 
         Scanner scanner = new Scanner(System.in);
-        System.out.println("1. Пассажир");
-        System.out.println("2. Администратор");
-        int x = scanner.nextInt();
+
 
         ArrayList<String> stations = new ArrayList<>();
         stations.add("Бобруйск");
         stations.add("Минск");
         stations.add("Жлобин");
+        stations.add("Борисов");
 
         Train train =new Train("11:30") {
             @Override
@@ -26,35 +23,54 @@ public class Main {
                 return super.getTime();
             }
         };
-        ArrayList<String> time = new ArrayList<>();
-        time.add(train.getTime());
-        time.add
+        ArrayList<String> timeTrain = new ArrayList<>();
+        timeTrain.add(train.getTime());
+        timeTrain.add("10:23");
+        timeTrain.add("03:14");
+        timeTrain.add("07:34");
+        timeTrain.add("14:33");
+        timeTrain.add("16:03");
+        timeTrain.add("19:40");
+        timeTrain.add("23:17");
 
-        switch (x){
-            case (1): {
-                System.out.println("Введите ваше имя");
-                scanner.next();
-                String passenger = scanner.nextLine();
-                Passenger person = new Passenger(passenger);
-                for(int i = 0; stations.size() > i; i++){
-                    System.out.println((i+1) + ". " + stations.get(i));
+        boolean isTrue = true;
+        while (isTrue) {
+
+            System.out.println("1. Пассажир");
+            System.out.println("2. Администратор");
+            int x = scanner.nextInt();
+
+            switch (x) {
+                case (1): {
+                    System.out.println("Введите ваше имя");
+                    scanner.next();
+                    String passenger = scanner.nextLine();
+                    Passenger person = new Passenger(passenger);
+                    for (int i = 0; stations.size() > i; i++) {
+                        System.out.println((i + 1) + ". " + stations.get(i));
+                    }
+                    System.out.println(passenger + "Выберите станцию назначения:");
+                    int buf = scanner.nextInt();
+                    System.out.println("Введите дату поездки\nПример - 01.01.2022");
+                    scanner.next();
+                    String date = scanner.nextLine();
+                    System.out.println("Введите время пездки\nПример - 01:01");
+                    scanner.next();
+                    String time = scanner.nextLine();
+
                 }
-                System.out.println(passenger + " выберите станцию назначения:");
-                int buf = scanner.nextInt();
-                System.out.println("Введите дату поездки\nПример - 01.01.2022");
-                scanner.next();
-                String date = scanner.nextLine();
-                System.out.println("Введите время пездки\nПример - 01:00");
-                scanner.next();
-//                String time = scanner.nextLine();
-            }
-            break;
+                break;
 
-            case (2): {
-                Administrator admin = new Administrator("Никита");
+                case (2): {
+                    Administrator admin = new Administrator("Никита");
 
+                }
+                break;
+                default: {
+                    System.out.println("Error!!!Попробуйте корректно ввести!");
+                }
+                break;
             }
-            break;
         }
 
     }
